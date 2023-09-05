4fff01640dc82d38aa463ff8363909b1c6915003
DSYASWANTH777 <dsyaswanth777@gmail.com>
2023-09-05 11:07:22 +0530

    Setting up the project

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Logo from "../../assets/icons/Untitled.png"
+const Header = () => {
+  return (
+    <div className='bg-slate-400'>
+      <img src={Logo}/>
+    </div>
+
+  )
+}
+
+export default Header
