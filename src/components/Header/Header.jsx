f5d4803bce4cb5061f7ed3d45b34dab4b7e2dcc0
DSYASWANTH777 <dsyaswanth777@gmail.com>
2023-09-05 23:58:37 +0530

    Add(Navabar)

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,36 @@
-import React from 'react'
-import Logo from "../../assets/icons/Untitled.png"
+import React from "react";
+import Logo from "../../assets/icons/brand_logo.svg";
+import { FiSearch, FiShoppingCart } from "react-icons/fi"; // Import FiSearch
+import { BsSuitHeart } from "react-icons/bs";
+
+import "./Header.scss";
+import { Badge, Input, InputGroup, InputGroupText } from "reactstrap";
+
 const Header = () => {
   return (
-    <div className='bg-slate-400'>
-      <img src={Logo}/>
+    <div className="  header shadow p-3 bg-white  ">
+      <div className="container">
+        <div className="d-flex justify-content-around align-items-center">
+          <h2 className="logo logo_text">
+            GSR <span className="fs-3 ">Handlooms</span>
+          </h2>
+          <div className="d-flex justify-content-center align-items-center gap-5">
+            <InputGroup className="input rounded border border-dark border-2">
+{/*               
+              <div className="d-flex justify-content-center align-items-center p-2">
+                <FiSearch size={20} /> 
+              </div> */}
+              <Input type="search" name="" id="" placeholder="Search" />
+            </InputGroup>
+            <div className=" d-flex gap-3">
+              <FiShoppingCart size={22} />
+              <BsSuitHeart size={22} />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
+  );
+};
 
-  )
-}
-
-export default Header
+export default Header;
